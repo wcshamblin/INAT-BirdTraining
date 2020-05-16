f1b53407d8342fa9ac09033c9a65411965941cc6
Will Shamblin <will@shamblin.org>
2020-05-15 20:20:10 -0400

    Initial commit

--- training.sh
+++ training.sh
@@ -1,8 +1,8 @@
-PIPELINE_CONFIG_PATH=/usr/xtmp/wcs/birdtraining/
+PIPELINE_CONFIG_PATH=/usr/xtmp/wcs/models/faster_rcnn_resnet101_fgvc_2018_07_19/pipeline.config
 MODEL_DIR=/usr/xtmp/wcs/models/inat2017/
 NUM_TRAIN_STEPS=10000
 SAMPLE_1_OF_N_EVAL_EXAMPLES=1
-python ./model_main.py \
+python3 object_detection/model_main.py \
     --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
     --model_dir=${MODEL_DIR} \
     --num_train_steps=${NUM_TRAIN_STEPS} \
